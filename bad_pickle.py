a454a650295451ea6950f98d2f1bef726b8579dd
zimkamartin <martin.zimka@gmail.com>
2023-02-21 12:35:08 +0100

    shortened line number 2

--- bad_pickle.py
+++ bad_pickle.py
@@ -1,5 +1,6 @@
 # contains bunch of buggy examples
-# taken from https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
+# taken from https://hackernoon.com/
+# 10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
 import cPickle
 import subprocess
 import base64
