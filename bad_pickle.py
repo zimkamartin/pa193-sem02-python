2025eb0361bb1fe7dac8b41fffc33a030c11e63c
zimkamartin <martin.zimka@gmail.com>
2023-02-21 12:44:40 +0100

    changed the order of imports

--- bad_pickle.py
+++ bad_pickle.py
@@ -1,10 +1,9 @@
 # contains bunch of buggy examples
 # taken from https://hackernoon.com/
 # 10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
+import subprocess
 import cPickle
-import subprocess
 import base64
-import subprocess
 
 
 # Input injection
